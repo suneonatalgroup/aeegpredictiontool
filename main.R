a59b7662acbeabb146f81a9f4a189815fe30f169
suneonatalgroup <83500375+suneonatalgroup@users.noreply.github.com>
2024-01-17 21:19:02 +0100

    use SWC instead of seizures in main.R

--- main.R
+++ main.R
@@ -17,48 +17,47 @@
 ## - aEEG_p12: aEEG score (1, 2, 3, 4 or 5) for the 12th time interval (66-72h)
 ## - aEEG_p13: aEEG score (1, 2, 3, 4 or 5) for the 13th time interval (72-78h)
 ## - aEEG_p14: aEEG score (1, 2, 3, 4 or 5) for the 14th time interval (78-84h)
-## - electrographic_seizure: TRUE or FALSE
-## - clinical_seizure: TRUE or FALSE
+## - swc: sleep-wake cycling coded as 0 for absent, 1 for abnormal, 2 for normal
 
 ## Example data structure:
 
 if (FALSE) {
   d <- data.table::fread("
-    S01 0 1 2 1 2 3 1 2 1 1 1 2 1 2 1 TRUE  FALSE
-    S02 1 3 4 3 2 4 1 2 2 3 2 1 2 3 2 FALSE FALSE
-    S03 0 2 1 1 2 1 2 1 1 2 2 2 2 1 1 TRUE  FALSE
-    S04 1 4 2 1 4 3 1 2 1 1 2 2 4 1 3 TRUE  TRUE
-    S05 1 3 4 3 2 4 1 2 3 2 2 2 2 3 1 FALSE FALSE
-    S06 1 3 2 3 2 4 1 2 1 3 2 1 2 3 2 FALSE FALSE
-    S07 0 2 1 1 2 1 2 1 2 1 2 1 2 1 1 TRUE  FALSE
-    S08 0 1 2 1 1 3 1 2 1 3 2 1 4 1 3 TRUE  FALSE
-    S09 1 1 2 1 4 3 1 2 1 2 2 1 4 1 1 TRUE  TRUE
-    S10 1 3 1 5 2 2 1 2 5 3 5 2 1 2 1 TRUE  TRUE
-    S11 0 1 3 1 2 3 1 3 1 1 1 2 2 2 2 TRUE  FALSE
-    S12 1 3 4 3 2 4 1 3 2 3 2 1 3 3 1 FALSE FALSE
-    S13 0 2 2 1 2 1 2 2 1 2 2 2 3 1 4 TRUE  TRUE
-    S14 1 4 3 1 4 3 1 3 1 1 2 2 5 1 1 TRUE  TRUE
-    S15 1 3 5 3 2 4 1 3 3 2 2 2 3 3 3 FALSE FALSE
-    S16 1 3 3 3 2 4 1 3 1 3 2 1 3 3 1 FALSE TRUE
-    S17 0 2 2 1 2 1 2 2 2 1 2 1 3 1 4 TRUE  FALSE
-    S18 0 1 3 1 1 3 1 3 1 3 2 1 5 1 1 FALSE FALSE
-    S19 1 1 4 1 4 3 1 3 1 2 2 1 5 1 2 FALSE TRUE
-    S20 1 3 2 5 2 2 1 3 5 3 5 2 2 2 1 TRUE  FALSE
-    S21 1 1 3 1 2 3 1 3 2 1 1 2 2 2 4 FALSE FALSE
-    S22 0 3 4 3 2 4 1 3 1 3 2 1 3 3 4 FALSE FALSE
-    S23 1 2 2 1 2 1 2 2 2 2 2 2 3 1 3 FALSE TRUE
-    S24 0 4 3 1 4 3 1 3 2 1 2 2 5 1 2 TRUE  TRUE
-    S25 0 3 5 3 2 4 1 3 1 2 2 2 3 3 2 TRUE  FALSE
-    S26 0 3 3 3 2 4 1 3 2 3 2 1 3 3 3 FALSE TRUE
-    S27 1 2 2 1 2 1 2 2 3 1 2 1 3 1 3 TRUE  TRUE
-    S28 1 1 3 1 1 3 1 3 2 3 2 1 5 1 4 FALSE FALSE
-    S29 0 1 4 1 4 3 1 3 3 2 2 1 5 1 5 TRUE  TRUE
-    S30 0 3 2 5 2 2 1 3 2 3 5 2 2 2 3 TRUE  FALSE
+    S01 0 1 2 1 2 3 1 2 1 1 1 2 1 2 1 2
+    S02 1 3 4 3 2 4 1 2 2 3 2 1 2 3 2 0
+    S03 0 2 1 1 2 1 2 1 1 2 2 2 2 1 1 1
+    S04 1 4 2 1 4 3 1 2 1 1 2 2 4 1 3 1
+    S05 1 3 4 3 2 4 1 2 3 2 2 2 2 3 1 0
+    S06 1 3 2 3 2 4 1 2 1 3 2 1 2 3 2 2
+    S07 0 2 1 1 2 1 2 1 2 1 2 1 2 1 1 2
+    S08 0 1 2 1 1 3 1 2 1 3 2 1 4 1 3 1
+    S09 1 1 2 1 4 3 1 2 1 2 2 1 4 1 1 2
+    S10 1 3 1 5 2 2 1 2 5 3 5 2 1 2 1 1
+    S11 0 1 3 1 2 3 1 3 1 1 1 2 2 2 2 2
+    S12 1 3 4 3 2 4 1 3 2 3 2 1 3 3 1 1
+    S13 0 2 2 1 2 1 2 2 1 2 2 2 3 1 4 2
+    S14 1 4 3 1 4 3 1 3 1 1 2 2 5 1 1 0
+    S15 1 3 5 3 2 4 1 3 3 2 2 2 3 3 3 0
+    S16 1 3 3 3 2 4 1 3 1 3 2 1 3 3 1 2
+    S17 0 2 2 1 2 1 2 2 2 1 2 1 3 1 4 1
+    S18 0 1 3 1 1 3 1 3 1 3 2 1 5 1 1 2
+    S19 1 1 4 1 4 3 1 3 1 2 2 1 5 1 2 1
+    S20 1 3 2 5 2 2 1 3 5 3 5 2 2 2 1 2
+    S21 1 1 3 1 2 3 1 3 2 1 1 2 2 2 4 0
+    S22 0 3 4 3 2 4 1 3 1 3 2 1 3 3 4 2
+    S23 1 2 2 1 2 1 2 2 2 2 2 2 3 1 3 0
+    S24 0 4 3 1 4 3 1 3 2 1 2 2 5 1 2 0
+    S25 0 3 5 3 2 4 1 3 1 2 2 2 3 3 2 1
+    S26 0 3 3 3 2 4 1 3 2 3 2 1 3 3 3 2
+    S27 1 2 2 1 2 1 2 2 3 1 2 1 3 1 3 0
+    S28 1 1 3 1 1 3 1 3 2 3 2 1 5 1 4 2
+    S29 0 1 4 1 4 3 1 3 3 2 2 1 5 1 5 1
+    S30 0 3 2 5 2 2 1 3 2 3 5 2 2 2 3 2
   ")
   data.table::setnames(d, c(
     "sid", "outcome",
     paste0("aEEG_p", formatC(1:14, width = 2, flag = "0")),
-    "electrographic_seizure", "clinical_seizure"))
+    "swc"))
 }
 
 
@@ -147,10 +146,10 @@ model_fit <- glm(outcome ~ ., data = dx, family = "binomial")
 model_eval(modelN, model_fit, dx)
 
 
-## Growth curve method extended with the `both_seizure_types` variable (7B model)
+## Growth curve method extended with the `swc` variable (7B model)
 
 modelN <- "7B"
-vars <- c("outcome", "aEEG_mean", "aEEG_slope", "both_seizure_types")
+vars <- c("outcome", "aEEG_mean", "aEEG_slope", "swc")
 dx <- na.omit(d[, ..vars])
 model_fit <- glm(outcome ~ ., data = dx, family = "binomial")
 model_eval(modelN, model_fit, dx)
@@ -159,7 +158,7 @@ model_eval(modelN, model_fit, dx)
 ## 7B model: 5-fold cross validation
 
 modelN <- "7B"
-vars <- c("outcome", "aEEG_mean", "aEEG_slope", "both_seizure_types")
+vars <- c("outcome", "aEEG_mean", "aEEG_slope", "swc")
 dx <- na.omit(d[, ..vars])
 set.seed(1)
 CV_AUC_CI(dx)
@@ -168,7 +167,7 @@ CV_AUC_CI(dx)
 ## 7B model: 5-fold cross validation repeated 1000 times
 
 modelN <- "7B"
-vars <- c("outcome", "aEEG_mean", "aEEG_slope", "both_seizure_types")
+vars <- c("outcome", "aEEG_mean", "aEEG_slope", "swc")
 dx <- na.omit(d[, ..vars])
 set.seed(1)
 auc_rep <- list()
